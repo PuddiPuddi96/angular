0fa4519b0dc9118f83fd66a4426be0f3112beaa6
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-07-30 17:49:54 +0200

    dev: reset project and add header component

---
+++ angular-course/first-angular-app/src/app/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+/*
+selector: tell Angular which elements on the screen should be controlled by our own component
+  or should be replaced by our own component
+standalone: this property marks this Component as a so-called Standalone Component, and Angular also knows other types of Components
+templateUrl: defines the markup, the content that should be displayed by that Component.
+
+
+*/
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: '../header/header.component.html',
+  styleUrl: '../header/header.component.css'
+})
+
+export class HeaderComponent { }

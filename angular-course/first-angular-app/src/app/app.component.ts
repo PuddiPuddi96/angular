742826451a27bcf25d822411a5a0f7cbda605e83
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-07-31 18:00:11 +0200

    dev: make user-component reusable

--- angular-course/first-angular-app/src/app/app.component.ts
+++ angular-course/first-angular-app/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -12,5 +13,6 @@ import { UserComponent } from "./user/user.component";
 })
 
 export class AppComponent {
+  users = DUMMY_USERS; //Expose import data in the template of this component
   title = 'first-angular-app';
 }

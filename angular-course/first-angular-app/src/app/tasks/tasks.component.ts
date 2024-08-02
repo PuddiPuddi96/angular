19d63f8b2614d98fe9eb4a312519387f1d7c0c6c
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-08-02 18:30:20 +0200

    dev: implement new-task component

--- angular-course/first-angular-app/src/app/tasks/tasks.component.ts
+++ angular-course/first-angular-app/src/app/tasks/tasks.component.ts
@@ -1,16 +1,19 @@
 import { Component, input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { DUMMY_TASKS } from '../dummy-data/dummy-tasks';
+import { NewTaskComponent } from './new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 
 export class TasksComponent {
+  isAddingTask = false;
+
   userId = input.required<string>()
   name = input.required<string>()
 
@@ -23,5 +26,13 @@ export class TasksComponent {
   onCompleteTask(id: string){
     console.log(id)
     this.tasks = this.tasks.filter((task) => task.id !== id)
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
+  }
+
+  onCancelAddTask() {
+    this.isAddingTask = false;
   }
 }

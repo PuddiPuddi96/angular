bb8c04ef30a4bd1c7c5553171671bdba6d079a18
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-08-02 14:41:48 +0200

    dev: add older implementation of for/if/else

--- angular-course/first-angular-app/src/app/app.component.ts
+++ angular-course/first-angular-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgFor, NgIf } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
@@ -8,7 +9,7 @@ import { TasksComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

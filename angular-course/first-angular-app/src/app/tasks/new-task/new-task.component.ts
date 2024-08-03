c6278c9ebfa0b66b06963a8a69847b6071937d7a
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-08-03 14:16:54 +0200

    dev: update form for new task

--- angular-course/first-angular-app/src/app/tasks/new-task/new-task.component.ts
+++ angular-course/first-angular-app/src/app/tasks/new-task/new-task.component.ts
@@ -1,17 +1,32 @@
-import { Component, output } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { type NewTask } from './new-task.model';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
   cancel = output();
+  add = output<NewTask>();
+
+  enteredTitle = signal('');
+  enteredSummary = signal('');
+  enteredDueDate = signal('');
 
   onCancel() {
     this.cancel.emit();
+  }
+
+  onSubmit() {
+    this.add.emit({
+      title: this.enteredTitle(),
+      summary: this.enteredSummary(),
+      date: this.enteredDueDate()
+    });
   }
 
 }

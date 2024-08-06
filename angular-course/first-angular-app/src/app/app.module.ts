76f173bcd3d8b826d450ebeaee0ece2b27d97ab0
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-08-06 17:03:21 +0200

    Fix: -

--- angular-course/first-angular-app/src/app/app.module.ts
+++ angular-course/first-angular-app/src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { TasksComponent } from "./tasks/tasks.component";
+import { BrowserModule } from "@angular/platform-browser";
 
 @NgModule({
   declarations: [AppComponent], //to declare and register all the components that need to work together
-  bootstrap: [AppComponent] //Root component
+  bootstrap: [AppComponent], //Root component
+  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent]
 })
 export class AppModule {
 

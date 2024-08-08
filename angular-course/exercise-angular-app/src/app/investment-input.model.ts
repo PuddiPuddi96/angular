d3c2166b3a169dda4a2a6b1ccc6d76dade441586
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-08-08 15:51:29 +0200

    ref: create model for form input

---
+++ angular-course/exercise-angular-app/src/app/investment-input.model.ts
@@ -0,0 +1,13 @@
+export type InvestmentInput = {
+  initialInvestment: number,
+  annualInvestment: number,
+  expectedReturn: number,
+  duration: number,
+}
+
+// export interface InvestmentInput {
+//   initialInvestment: number,
+//   annualInvestment: number,
+//   expectedReturn: number,
+//   duration: number,
+// }
--- angular-course/exercise-angular-app/src/app/user-input/user-input.component.ts
+++ angular-course/exercise-angular-app/src/app/user-input/user-input.component.ts
@@ -1,5 +1,6 @@
 import { Component, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { type InvestmentInput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
@@ -9,13 +10,8 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-  //@Output() calculate = new EventEmitter();
-  calculate = output<{
-    initialInvestment: number,
-    annualInvestment: number,
-    expectedReturn: number,
-    duration: number,
-  }>();
+  //@Output() calculate = new EventEmitter<InvestmentInput>();
+  calculate = output<InvestmentInput>();
 
   enteredInitialInvestment = signal('1');
   enteredAnnualInvestment = signal('2');

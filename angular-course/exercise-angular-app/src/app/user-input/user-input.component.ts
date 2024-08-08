20ad723ef2d1afc00a5755bf835c00475f132219
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-08-08 15:40:34 +0200

    dev: update form

--- angular-course/exercise-angular-app/src/app/user-input/user-input.component.ts
+++ angular-course/exercise-angular-app/src/app/user-input/user-input.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  //@Output() calculate = new EventEmitter();
+  calculate = output<{
+    initialInvestment: number,
+    annualInvestment: number,
+    expectedReturn: number,
+    duration: number,
+  }>();
+
+  enteredInitialInvestment = signal('1');
+  enteredAnnualInvestment = signal('2');
+  enteredExpectedReturn = signal('3');
+  enteredDuration = signal('4');
+
+  onSubmit() {
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment(),
+      duration: +this.enteredDuration(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      annualInvestment: +this.enteredAnnualInvestment()
+    });
+  }
 
 }

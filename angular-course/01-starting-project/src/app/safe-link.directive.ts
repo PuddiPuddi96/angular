da674c1331143168833ef2686c3b2561c27c7eea
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-09-16 16:08:31 +0200

    dev: inject hostElement

--- angular-course/01-starting-project/src/app/safe-link.directive.ts
+++ angular-course/01-starting-project/src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, input } from "@angular/core";
+import { Directive, ElementRef, inject, input } from "@angular/core";
 
 @Directive({
   selector: 'a[appSafeLink]',
@@ -13,13 +13,14 @@ export class SafeLinkDirective {
   }
 
   queyParam = input('myapp');
+  private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
 
   onConfirmLeavePage(event: MouseEvent) {
     const wantsToLeave = window.confirm('Do you want to leave the app?')
 
     if(wantsToLeave){
-      const address = (event.target as HTMLAnchorElement).href;
-      (event.target as HTMLAnchorElement).href = address + '?from=' + this.queyParam();
+      const address = this.hostElementRef.nativeElement.href;
+      this.hostElementRef.nativeElement.href = address + '?from=' + this.queyParam();
       return;
     }
 

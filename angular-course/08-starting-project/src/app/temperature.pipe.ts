fd66ef18dd98af1be8cb65719685ffb78e2fe2e0
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-09-18 16:07:20 +0200

    dev: implement simple transfrom (from Celsius to Fahrenheit

--- angular-course/08-starting-project/src/app/temperature.pipe.ts
+++ angular-course/08-starting-project/src/app/temperature.pipe.ts
@@ -4,10 +4,20 @@ import { Pipe, PipeTransform } from "@angular/core";
   name: 'temp',
   standalone: true
 })
-export class TemperaturePipe implements PipeTransform{
-  transform(value: any, ...args: any[]) {
-    return value + ' - transformed';
+export class TemperaturePipe implements PipeTransform {
+  transform(value: string | number) {
+    let numberValue: number;
+
+    if (typeof value === 'string') {
+      numberValue = parseFloat(value);
+    } else {
+      numberValue = value;
+    }
+
+    //From Celsius to Fahrenheit
+    const outputTemp = numberValue * (9 / 5) + 32;
+
+    return `${outputTemp} °F`;
   }
-  
 
 }

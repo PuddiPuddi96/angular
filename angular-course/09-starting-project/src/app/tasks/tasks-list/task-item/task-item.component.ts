061f55c5d9b5610ea10b9e699b3c874a0a05288a
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-09-24 15:01:01 +0200

    dev: import method to update ticket's status

--- angular-course/09-starting-project/src/app/tasks/tasks-list/task-item/task-item.component.ts
+++ angular-course/09-starting-project/src/app/tasks/tasks-list/task-item/task-item.component.ts
@@ -1,7 +1,8 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { Task, TaskStatus } from '../../task.model';
+import { TasksService } from '../../tasks.service';
 
 @Component({
   selector: 'app-task-item',
@@ -11,6 +12,8 @@ import { Task, TaskStatus } from '../../task.model';
   styleUrl: './task-item.component.css',
 })
 export class TaskItemComponent {
+  private tasksService = inject(TasksService);
+
   task = input.required<Task>();
   taskStatus = computed(() => {
     switch (this.task().status) {
@@ -29,9 +32,6 @@ export class TaskItemComponent {
     let newStatus: TaskStatus = 'OPEN';
 
     switch (status) {
-      case 'open':
-        newStatus = 'OPEN';
-        break;
       case 'in-progress':
         newStatus = 'IN_PROGRESS';
         break;
@@ -41,5 +41,7 @@ export class TaskItemComponent {
       default:
         break;
     }
+
+    this.tasksService.updateTaskStatus(taskId, newStatus);
   }
 }

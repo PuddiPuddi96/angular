0ac0f12be54a3bc303eeb7b698ba710816a2a39c
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-10-05 10:39:16 +0200

    dev: handle response in interceptor

--- angular-course/12-http/src/main.ts
+++ angular-course/12-http/src/main.ts
@@ -1,7 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpEventType, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 function loggingInterceptor(
   request: HttpRequest<unknown>,
@@ -10,7 +11,17 @@ function loggingInterceptor(
   console.log('[Outgoing Request]');
   console.log(request);
 
-  return next(request);
+  return next(request).pipe(
+    tap({
+      next: event => {
+        if(event.type === HttpEventType.Response) {
+          console.log('[Outgoing Response]');
+          console.log(event.status);
+          console.log(event.body);
+        }
+      }
+    })
+  );
 }
 
 bootstrapApplication(AppComponent, {

71b5dfeca6e04160ab544018015e50b8608b1e9c
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-10-07 22:10:00 +0200

    dev: improve the form storage

--- angular-course/13-forms/src/app/auth/login/login.component.ts
+++ angular-course/13-forms/src/app/auth/login/login.component.ts
@@ -16,6 +16,22 @@ export class LoginComponent {
 
   constructor() {
     afterNextRender(() => {
+      const savedForm = window.localStorage.getItem('saved-login-form');
+
+      if(savedForm) {
+        const loadedFormData = JSON.parse(savedForm);
+        const savedEmail = loadedFormData.email;
+
+        setTimeout(() => {
+          this.form().controls['email'].setValue(savedEmail);
+
+          // this.form().setValue({
+          //   email: savedEmail,
+          //   password: ''
+          // });
+        }, 1);
+      }
+
       const subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
         next: (value) =>
           window.localStorage.setItem(

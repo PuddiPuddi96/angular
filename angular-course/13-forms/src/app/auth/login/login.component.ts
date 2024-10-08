f83e4a98fd36d100fb85fcc5f9cc24b6ef7638ba
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-10-08 08:56:47 +0200

    dev: connect temaplate with component

--- angular-course/13-forms/src/app/auth/login/login.component.ts
+++ angular-course/13-forms/src/app/auth/login/login.component.ts
@@ -63,11 +63,27 @@
 
 //*** REACTIVE FORM ***/
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  onSubmit() {
+    console.log(this.loginForm);
+
+    const enteredEmail = this.loginForm.value.email;
+    const enteredPassword = this.loginForm.value.password;
+
+    console.log(enteredEmail, enteredPassword);
+  }
+}

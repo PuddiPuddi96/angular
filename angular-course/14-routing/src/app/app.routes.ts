e9bb852be119866f2e3172633d248d9d58d95495
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-10-14 19:58:34 +0200

    dev: add route for no task

--- angular-course/14-routing/src/app/app.routes.ts
+++ angular-course/14-routing/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from "@angular/router";
-import { TaskComponent } from "./tasks/task/task.component";
+import { TasksComponent } from "./tasks/tasks.component";
+import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 
 export const routes: Routes = [
   {
+    path: '',
+    component: NoTaskComponent
+  },
+  {
     path: 'tasks',
-    component: TaskComponent,
+    component: TasksComponent,
   },
 ];

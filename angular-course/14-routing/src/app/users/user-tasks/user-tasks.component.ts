b67d8c4a11a5867553ce0873ab46169bdda17f7a
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-10-19 19:12:05 +0200

    dev: etracting dynamic route params via Obs

--- angular-course/14-routing/src/app/users/user-tasks/user-tasks.component.ts
+++ angular-course/14-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,5 +1,24 @@
-import { Component, computed, inject, input } from '@angular/core';
+// import { Component, computed, inject, input } from '@angular/core';
+// import { UsersService } from '../users.service';
+
+// @Component({
+//   selector: 'app-user-tasks',
+//   standalone: true,
+//   templateUrl: './user-tasks.component.html',
+//   styleUrl: './user-tasks.component.css',
+// })
+// export class UserTasksComponent {
+//   userId = input.required<string>();
+//   private usersService = inject(UsersService);
+
+//   userName = computed(
+//     () => this.usersService.users.find((u) => u.id === this.userId())?.name
+//   );
+// }
+
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { UsersService } from '../users.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
@@ -7,11 +26,24 @@ import { UsersService } from '../users.service';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {
-  userId = input.required<string>();
-  private usersService = inject(UsersService);
+export class UserTasksComponent implements OnInit {
+  userName = '';
 
-  userName = computed(
-    () => this.usersService.users.find((u) => u.id === this.userId())?.name
-  );
+  private usersService = inject(UsersService);
+  private activatedRoute = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    console.log(this.activatedRoute);
+    
+    const subscription = this.activatedRoute.paramMap.subscribe({
+      next: (paramMap) => {
+        this.userName =
+          this.usersService.users.find((u) => u.id === paramMap.get('userId'))
+            ?.name || '';
+      },
+    });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe())
+  }
 }

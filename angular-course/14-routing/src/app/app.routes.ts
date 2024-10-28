9b579f7f7e4756766fcdf543d17f41f0b7987a0e
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-10-28 22:29:43 +0100

    dev: use resolve function to get data from url

--- angular-course/14-routing/src/app/app.routes.ts
+++ angular-course/14-routing/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
+import { resolveUserName, UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { routes as userRoutes} from './users/users.routes'
 import { NotFoundComponent } from './not-found/not-found.component';
 
@@ -14,7 +14,10 @@ export const routes: Routes = [
     component: UserTasksComponent,
     children: userRoutes,
     data: {
-      messag: 'Hello!'
+      message: 'Hello!'
+    },
+    resolve: {
+      userName: resolveUserName
     }
   },
   {
--- angular-course/14-routing/src/app/users/user-tasks/user-tasks.component.ts
+++ angular-course/14-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -16,9 +16,44 @@
 //   );
 // }
 
-import { Component, DestroyRef, inject, input, OnInit } from '@angular/core';
+// import { Component, DestroyRef, inject, input, OnInit } from '@angular/core';
+// import { UsersService } from '../users.service';
+// import { ActivatedRoute, ActivatedRouteSnapshot, ResolveFn, RouterLink, RouterOutlet, RouterStateSnapshot } from '@angular/router';
+
+// @Component({
+//   selector: 'app-user-tasks',
+//   standalone: true,
+//   imports: [RouterOutlet, RouterLink],
+//   templateUrl: './user-tasks.component.html',
+//   styleUrl: './user-tasks.component.css',
+// })
+// export class UserTasksComponent implements OnInit {
+//   userName = '';
+//   message= input.required<string>();
+
+//   private usersService = inject(UsersService);
+//   private activatedRoute = inject(ActivatedRoute);
+//   private destroyRef = inject(DestroyRef);
+
+//   ngOnInit(): void {
+//     console.log('Input data: ' + this.message());
+//     console.log(this.activatedRoute);
+    
+//     const subscription = this.activatedRoute.paramMap.subscribe({
+//       next: (paramMap) => {
+//         this.userName =
+//           this.usersService.users.find((u) => u.id === paramMap.get('userId'))
+//             ?.name || '';
+//       },
+//     });
+
+//     this.destroyRef.onDestroy(() => subscription.unsubscribe())
+//   }
+// }
+
+import { Component, inject, input } from '@angular/core';
 import { UsersService } from '../users.service';
-import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
+import { ActivatedRouteSnapshot, ResolveFn, RouterLink, RouterOutlet, RouterStateSnapshot } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
@@ -27,26 +62,17 @@ import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent implements OnInit {
-  userName = '';
-  message= input.required<string>();
-
-  private usersService = inject(UsersService);
-  private activatedRoute = inject(ActivatedRoute);
-  private destroyRef = inject(DestroyRef);
-
-  ngOnInit(): void {
-    console.log('Input data: ' + this.message());
-    console.log(this.activatedRoute);
-    
-    const subscription = this.activatedRoute.paramMap.subscribe({
-      next: (paramMap) => {
-        this.userName =
-          this.usersService.users.find((u) => u.id === paramMap.get('userId'))
-            ?.name || '';
-      },
-    });
-
-    this.destroyRef.onDestroy(() => subscription.unsubscribe())
-  }
+export class UserTasksComponent {
+  userName = input.required<string>();
+  message = input.required<string>();
 }
+
+export const resolveUserName: ResolveFn<string> = (
+  activatedRoute: ActivatedRouteSnapshot,
+  routerState: RouterStateSnapshot
+) => {
+  const usersService = inject(UsersService);
+  const userName = usersService.users.find((u) => u.id === activatedRoute.paramMap.get('userId'))
+  ?.name || '';
+  return userName;
+};

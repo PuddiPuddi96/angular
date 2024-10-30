2d2a46fa2c5a0be1c4d9b5e2e8d352e7478671ce
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-10-30 17:52:00 +0100

    dev: setting e resolving title

--- angular-course/14-routing/src/app/app.routes.ts
+++ angular-course/14-routing/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { resolveUserName, UserTasksComponent } from './users/user-tasks/user-tasks.component';
+import { resolveTitle, resolveUserName, UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { routes as userRoutes} from './users/users.routes'
 import { NotFoundComponent } from './not-found/not-found.component';
 
@@ -8,6 +8,7 @@ export const routes: Routes = [
   {
     path: '', // <your domain>/
     component: NoTaskComponent,
+    title: 'No task selected'
   },
   {
     path: 'users/:userId', //<your-domain>/users/<uid>
@@ -18,7 +19,8 @@ export const routes: Routes = [
     },
     resolve: {
       userName: resolveUserName
-    }
+    },
+    title: resolveTitle
   },
   {
     path: '**',
--- angular-course/14-routing/src/app/users/user-tasks/user-tasks.component.ts
+++ angular-course/14-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -85,3 +85,10 @@ export const resolveUserName: ResolveFn<string> = (
   ?.name || '';
   return userName;
 };
+
+export const resolveTitle: ResolveFn<string> = (
+  activatedRoute: ActivatedRouteSnapshot,
+  routerState: RouterStateSnapshot
+)=> {
+  return resolveUserName(activatedRoute, routerState) + '\'s Tasks'
+}

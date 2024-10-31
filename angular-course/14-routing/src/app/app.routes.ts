3735750e6c85f7e298389dd3c2fb73089c768197
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-10-31 20:56:43 +0100

    dev: add dummt route guard

--- angular-course/14-routing/src/app/app.routes.ts
+++ angular-course/14-routing/src/app/app.routes.ts
@@ -1,8 +1,20 @@
-import { Routes } from '@angular/router';
+import { CanMatchFn, RedirectCommand, Route, Router, Routes, UrlSegment } from '@angular/router';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { resolveTitle, resolveUserName, UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { routes as userRoutes} from './users/users.routes'
 import { NotFoundComponent } from './not-found/not-found.component';
+import { inject } from '@angular/core';
+
+const dummyCanMatch: CanMatchFn = (route: Route, segments: UrlSegment[]) => {
+  const router = inject(Router);
+  const shouldGetAccess = Math.random();
+
+  if(shouldGetAccess < 0.5) {
+    return true;
+  }
+
+  return new RedirectCommand(router.parseUrl('/unauthorized'))
+}
 
 export const routes: Routes = [
   {
@@ -14,6 +26,7 @@ export const routes: Routes = [
     path: 'users/:userId', //<your-domain>/users/<uid>
     component: UserTasksComponent,
     children: userRoutes,
+    canMatch: [dummyCanMatch],
     data: {
       message: 'Hello!'
     },

b094f10c5bfcec7912b75f4453fd103eadbfbfd0
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2025-02-27 21:08:59 +0100

    [A][FEAT]: Update eployee component

--- angular-ngrx-crud/src/app/component/employee/employee.component.ts
+++ angular-ngrx-crud/src/app/component/employee/employee.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardModule} from '@angular/material/card'
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })

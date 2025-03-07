e8b52ef8f66f84e7146ec23f9b374d2023825472
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2025-03-07 22:50:30 +0100

    [A][FEAT]: Add simple CRUD

--- angular-ngrx-crud/src/app/service/employee.service.ts
+++ angular-ngrx-crud/src/app/service/employee.service.ts
@@ -1,8 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Employee } from '../model/employee';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeService {
-  constructor() {}
+  apiUrl = 'http://localhost:3000/employee';
+
+  constructor(private http: HttpClient) {}
+
+  public getAll() {
+    return this.http.get<Employee[]>(this.apiUrl);
+  }
+
+  public getEmployeeById(idEmployee: number) {
+    return this.http.get<Employee>(this.apiUrl + '/' + idEmployee);
+  }
+
+  public createEmployee(data: Employee) {
+    return this.http.post(this.apiUrl, data);
+  }
+
+  public updateEmployee(data: Employee) {
+    return this.http.put(this.apiUrl + '/' + data.id, data);
+  }
+
+  public deleteEmployee(idEmployee: number) {
+    return this.http.delete(this.apiUrl + '/' + idEmployee);
+  }
 }

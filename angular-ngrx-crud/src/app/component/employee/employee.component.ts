d12bc9fbea10b30ace06c6c8518b881beb6cf35d
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2025-03-09 21:19:57 +0100

    [A][FEAT]: Create table to show data

--- angular-ngrx-crud/src/app/component/employee/employee.component.ts
+++ angular-ngrx-crud/src/app/component/employee/employee.component.ts
@@ -1,18 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { AddEmployeeComponent } from '../add-employee/add-employee.component';
+import { Employee } from '../../model/employee';
+import { EmployeeService } from '../../service/employee.service';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatDialogModule],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatDialogModule,
+    CommonModule,
+    MatTableModule,
+  ],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css',
 })
-export class EmployeeComponent {
-  constructor(private dialog: MatDialog) {}
+export class EmployeeComponent implements OnInit, OnDestroy {
+  employeeList: Employee[] = [];
+  dataSource!: MatTableDataSource<Employee>;
+  displayedColumns: string[] = [
+    'id',
+    'name',
+    'role',
+    'doj',
+    'salary',
+    'action',
+  ];
+  subscription = new Subscription();
+
+  constructor(
+    private readonly dialog: MatDialog,
+    private readonly service: EmployeeService
+  ) {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  public getAllEmployee() {
+    let subscribe = this.service.getAll().subscribe((item) => {
+      this.employeeList = item;
+      this.dataSource = new MatTableDataSource(this.employeeList);
+    });
+    this.subscription.add(subscribe);
+  }
 
   public addEmployee() {
     this.dialog.open(AddEmployeeComponent, {

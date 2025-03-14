ad3bc0776bc0ddd167605ebaf9ef747277e3d49b
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2025-03-14 20:15:12 +0100

    [A][FIX] fix action

--- angular-ngrx-crud/src/app/store/employee.action.ts
+++ angular-ngrx-crud/src/app/store/employee.action.ts
@@ -1,16 +1,16 @@
 import { createAction, props } from '@ngrx/store';
 import { Employee } from '../model/employee';
 
-export const LOAD_EMPLOYEE = 'employee gatAll';
+export const LOAD_EMPLOYEE = '[employee] getAll';
 export const LOAD_EMPLOYEE_SUCCESS = 'employee getAll suc';
 export const LOAD_EMPLOYEE_FAIL = 'employee getAll fail';
 
-const loadEmployee = createAction(LOAD_EMPLOYEE);
-const loadEmployeeSuc = createAction(
+export const loadEmployee = createAction(LOAD_EMPLOYEE);
+export const loadEmployeeSuc = createAction(
   LOAD_EMPLOYEE_SUCCESS,
-  props<{ list: Employee[] }>
+  props<{ list: Employee[] }>()
 );
-const loadEmployeeFail = createAction(
+export const loadEmployeeFail = createAction(
   LOAD_EMPLOYEE_FAIL,
-  props<{ errMss: string }>
+  props<{ errMsg: string }>()
 );

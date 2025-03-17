2ac319b791a2561e4af108286cdc644efcee7e6e
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2025-03-17 11:23:12 +0100

    [A][FEAT]: Implement reducer

--- angular-ngrx-crud/src/app/store/employee.reducer.ts
+++ angular-ngrx-crud/src/app/store/employee.reducer.ts
@@ -1,7 +1,16 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
 import { employeeState } from './employee.state';
+import { loadEmployeeFail, loadEmployeeSuc } from './employee.action';
 
-const _employeeReducer = createReducer(employeeState);
+const _employeeReducer = createReducer(
+  employeeState,
+  on(loadEmployeeSuc, (state, action) => {
+    return { ...state, list: action.list, errorMessage: '' };
+  }),
+  on(loadEmployeeFail, (state, action) => {
+    return { ...state, list: [], errorMessage: action.errMsg };
+  })
+);
 
 export function employeeReducer(state: any, action: any) {
   return _employeeReducer(state, action);

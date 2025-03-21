88a3977ac4ae098202fb0a8ed873a725c78f414b
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2025-03-21 20:34:54 +0100

    [A][FEAT]: Add delete action

--- angular-ngrx-crud/src/app/store/employee.action.ts
+++ angular-ngrx-crud/src/app/store/employee.action.ts
@@ -1,10 +1,16 @@
 import { createAction, props } from '@ngrx/store';
 import { Employee } from '../model/employee';
 
+//LOAD
 export const LOAD_EMPLOYEE = '[employee] getAll';
 export const LOAD_EMPLOYEE_SUCCESS = 'employee getAll suc';
 export const LOAD_EMPLOYEE_FAIL = 'employee getAll fail';
 
+//DELETE
+export const DELETE_EMPLOYEE = '[employee] delete';
+export const DELETE_EMPLOYEE_SUCCESS = '[employee] delete suc';
+
+//LOAD
 export const loadEmployee = createAction(LOAD_EMPLOYEE);
 export const loadEmployeeSuc = createAction(
   LOAD_EMPLOYEE_SUCCESS,
@@ -14,3 +20,16 @@ export const loadEmployeeFail = createAction(
   LOAD_EMPLOYEE_FAIL,
   props<{ errMsg: string }>()
 );
+
+//DELETE
+export const deleteEmployee = createAction(
+  DELETE_EMPLOYEE,
+  props<{ employeeId: number }>()
+);
+export const deleteEmployeeSuc = createAction(
+  DELETE_EMPLOYEE_SUCCESS,
+  props<{ employeeId: number }>()
+);
+
+//Empty
+export const emptyAction = createAction('empty');

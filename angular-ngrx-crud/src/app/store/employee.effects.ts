f446e01ad92b1a7c3ed4546ad5068c187833ffdb
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2025-03-22 14:21:07 +0100

    [A][FEAT]: Add delete action to effects

--- angular-ngrx-crud/src/app/store/employee.effects.ts
+++ angular-ngrx-crud/src/app/store/employee.effects.ts
@@ -2,16 +2,21 @@ import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { EmployeeService } from '../service/employee.service';
 import {
+  deleteEmployee,
+  deleteEmployeeSuc,
+  emptyAction,
   loadEmployee,
   loadEmployeeFail,
   loadEmployeeSuc,
 } from './employee.action';
-import { catchError, exhaustMap, map, of } from 'rxjs';
+import { catchError, exhaustMap, map, of, switchMap } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class EmployeeEffect {
   actions$ = inject(Actions);
   service = inject(EmployeeService);
+  toastr = inject(ToastrService);
 
   _loadEmployee = createEffect(() =>
     this.actions$.pipe(
@@ -26,4 +31,30 @@ export class EmployeeEffect {
       })
     )
   );
+
+  _deleteEmployee = createEffect(() =>
+    this.actions$.pipe(
+      ofType(deleteEmployee),
+      switchMap((action) => {
+        return this.service.deleteEmployee(action.employeeId).pipe(
+          switchMap((data) => {
+            return of(
+              deleteEmployeeSuc({ employeeId: action.employeeId }),
+              this.showAlert('Deleted succesdsfully', 'pass')
+            );
+          }),
+          catchError((err) => of(this.showAlert(err.message, 'fail')))
+        );
+      })
+    )
+  );
+
+  private showAlert(message: string, response: string) {
+    if (response === 'pass') {
+      this.toastr.success(message);
+    } else {
+      this.toastr.error(message);
+    }
+    return emptyAction();
+  }
 }
